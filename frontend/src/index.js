adefcd919bff607df9f28305f3f925fc9098be3e
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2022-09-19 16:25:26 +0100

    First Commit

---
+++ frontend/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'; // Importing React
+import ReactDOM from 'react-dom/client';  // Importing necessary things of React DOM
+import './index.css';
+import App from './App';  // Import App from App.js
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
