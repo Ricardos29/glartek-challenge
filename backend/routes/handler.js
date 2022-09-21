9b1a542a028b8f84c0d6f2394198912de320fcd2
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2022-09-21 10:51:31 +0100

    Get temperature and save on cache

--- backend/routes/handler.js
+++ backend/routes/handler.js
@@ -1,14 +1,77 @@
 const express = require('express');
 const router = express.Router();
+const fetch = require('node-fetch');
+const nodecache = require('node-cache');
+
+const appCache = new nodecache({stdTTL: 3599, checkperiod: 1800});
+const API_KEY = '4b0bda5bac2c84b7ab2705bbb0d1c739';
+const cities = [
+    {
+        'id': '2267056', 
+        'name': 'Lisboa',
+        'data' : []
+    }, 
+    {
+        'id': '2267094', 
+        'name': 'Leiria',
+        'data' : []
+    }, 
+    {
+        'id': '2740636', 
+        'name': 'Coimbra',
+        'data' : []
+    }, 
+    {
+        'id': '2735941', 
+        'name': 'Porto',
+        'data' : []
+    }, 
+    {
+        'id': '2268337', 
+        'name': 'Faro',
+        'data' : []
+    }, 
+]
 
 // GET temperature
-router.get('/temperature', (req, res) => {
-    const str = [{
-        "name": "Ricardo",
-        "msg": "This is my tweet"
-    }];
+router.get('/temperature/:location', (req, res) => {
+    if(appCache.has(req.params.location)) // Get temperature from Node Cache
+    {
+        console.log("Get temperature from Node Cache");
+        return res.send(appCache.get(req.params.location));
+    }
+    else
+    {
+        const url = `http://api.openweathermap.org/data/2.5/weather?id=${req.params.location}&APPID=${API_KEY}`;
+        
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            appCache.set(req.params.location, data)
+            res.send(data)
+        });
+    }
+});
 
-    res.end(JSON.stringify(str));
+router.get('/stats', (req, res) => {
+    res.send(appCache.getStats());
 });
 
 module.exports = router;
+
+// router.get('/temperatures/all', async (req, res ) => {
+
+//     const temp = await cities.forEach(city => {
+//         const url = `http://api.openweathermap.org/data/2.5/weather?id=${city.id}&APPID=${API_KEY}`;
+        
+//         fetch(url)
+//         .then(response => response.json())
+//         .then(data => {
+//             city.name = 'ola';
+//             console.log(city.name)
+//         })
+//     });
+
+//     res.end(JSON.stringify(cities));
+    
+// })
