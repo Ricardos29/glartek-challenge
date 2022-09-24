c21341d69bddde4e13758d8160fb4fcdedbc5a15
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2022-09-24 15:39:39 +0100

    Bg images and icons changes dynamically. Errors added. CSS to frontend added

--- frontend/src/components/Temperature.js
+++ frontend/src/components/Temperature.js
@@ -1,8 +1,23 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useLayoutEffect} from 'react';
 
 function Temperature() {
     const [temp, setTemp] = useState([]);
     const [location, setLocation] = useState('2267056'); // First location
+    const [bg, setBg] = useState('');
+    const [error, setError] = useState('');
+
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    const month = ["Jan","Feb","Mar","Apr","May","Jun","Jul", "Aug","Sep","Oct","Nov","Dec"];
+
+    const images = {};
+    images['Clear'] = { 'bg': './img/clear-bg.jpg' };
+    images['Clouds'] = { 'bg': './img/clouds-bg.jpg' };
+    images['Rain'] = { 'bg': './img/rain-bg.jpg' };
+    images['Thunderstorm'] = { 'bg': './img/strom-bg.jpg' };
+    images['Other'] = { 'bg': './img/clear-bg.jpg' };
+
+    const current = new Date();
+    const currentDate = `${current.getHours()}:${current.getMinutes()} - ${weekday[current.getDay()]},  ${current.getDate()} ${month[current.getMonth()]} ${current.getFullYear()}`;
 
     // List of locations
     const locations = [
@@ -26,53 +41,89 @@ function Temperature() {
             'value': '2268337', 
             'label': 'Faro',
         },
+        {
+            'value': 'invalid', 
+            'label': 'Invalid',
+        },
     ]
- 
-    const fetchTemperature = async() => { // GET temperature from backend
-        const data = await fetch(`/temperature/${location}`);
-        const temp = await data.json();
-        setTemp(temp);
-    }
     
-    useEffect(() => { // Call fetchTemperature when start app
-        fetchTemperature();
+    useLayoutEffect(() => { // Call fetchTemperature when start app
+        changeLocation(location);
     }, []);
 
-    setInterval(fetchTemperature, 1800000); // Call fetchTemperature 30 in 30 minutes
+    useEffect(() => { // Call fetchTemperature when start app
+        const interval = setInterval(() => {
+            changeLocation(location);
+        }, 1800000);
+
+        return () => clearInterval(interval); // Call fetchTemperature 30 in 30 minutes
+    }, [location]);
 
     const changeLocation = async (value) => { // Get temperature from backend of another location
+        setError(null) // Error to null
         setLocation(value);
         const data = await fetch(`/temperature/${value}`)
-        const temp = await data.json();
+        const temperature = await data.json();
+        if(temperature.cod === 200){
+            setTemp(temperature); // Set temp
+            if(images[temperature.weather[0].main]) {
+                setBg(images[temperature.weather[0].main]['bg']);
+            } 
+            else {
+                setBg(images['Other']['bg']);
+            }
+        } 
+        else {
+            errorHandler(temperature);
+        }
+    }
 
-        setTemp(temp);
+    const errorHandler = (temperature) => {
+        setBg('./img/something_went_wrong.jpg');
+        setError(temperature); // Error to temp with error code and message
+        setTemp(null); // Temp to null
     }
 
     return (
-        <div className="grid-container h-100">
-            
-            <div className="grid-item-left">
-                <section>
-                    {
-                        temp && temp.main && temp.main.temp && // IF statement
-                        <p>{(temp.main.temp - 273.15).toFixed(2)}º</p>
-                    }
+        <div className="grid-container h-100" style={{ backgroundImage: "url(" + bg + ")"}}>
+            <div className="grid-item-left d-flex">
+                { temp &&
+                    <section className="mt-auto mb-50px w-100">
+                        <div className="row align-center">
+                            <div className="col-bigger">
+                                {
+                                    temp.main && temp.main.temp && // IF statement
+                                    <p className="text-right temp">{(temp.main.temp - 273.15).toFixed(0)}º</p>
+                                }
+                            </div>
 
-                    {
-                        temp && temp.name && // IF statement
-                        <p>{temp.name}</p>
-                    }
+                            <div className="col-bigger">
+                                {
+                                    
+                                    temp.name && // IF statement
+                                    <p className="city">{temp.name}</p>
+                                }
+                                <p className="date">{currentDate}</p>
+                            </div>
 
-                    {
-                        temp && temp.weather && temp.weather[0].main &&// IF statement
-                        <p>{temp.weather[0].main}</p>
-                    }
+                            <div className="col-bigger text-left">
+                                {
+                                    temp.weather && temp.weather[0].icon &&// IF statement
+                                    <img alt="Weather condition icon" className="w-100px" src={'http://openweathermap.org/img/wn/' + temp.weather[0].icon + '@2x.png'} />
+                                }
+                                {
+                                    temp.weather && temp.weather[0].main &&// IF statement
+                                    <p className="text-left state">{temp.weather[0].main}</p>
+                                }
+                            </div>
+                        </div>
 
-                </section>
+                    </section>
+                }   
             </div>
 
             <div className="grid-item-right">
-                <form>
+                <div className="items-box">
                     <select value={location} onChange={(e) => changeLocation(e.target.value)}>
                         {locations.map((option, index) => (
                             <option key={index} value={option.value}>
@@ -80,86 +131,97 @@ function Temperature() {
                             </option>
                         ))}
                     </select>
-                </form>
+                    <hr className="hr"></hr>
+                </div>
 
-                    <p>{ location }</p>
-
+                { error && 
                     <section className="items-box">
-                
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Temp. Felt</p>
-                            </div>
-                            <div className="d-grid-3">
-                                {
-                                    temp && temp.main && temp.main.feels_like && // IF statement
-                                    <p className="text-right">{(temp.main.feels_like - 273.15).toFixed(2)}º</p>
-                                }
-                            </div>
-                        </div>
+                        <p className="bold text-left mb-40px">Error</p>
+                        <p className="text-left">Error: {error.cod}</p>
+                        <p className="text-left">Message: {error.message}</p>            
+                    </section>
+                }
 
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Temp. Minimum</p>
-                            </div>
-                            <div className="d-grid-3">                        
-                                {
-                                    temp && temp.main && temp.main.temp_min && // IF statement
-                                    <p className="text-right">{(temp.main.temp_min - 273.15).toFixed(2)}º</p>
-                                }
-                            </div>
+                { temp &&
+                <section className="items-box mt-auto mb-50px">
+                    <hr className="hr"></hr>
+                        
+                    <p className="bold text-left mb-40px">Weather Details</p>
+
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Felt</p>
                         </div>
+                        <div className="d-grid-3">
+                            {
+                                temp && temp.main && temp.main.feels_like && // IF statement
+                                <p className="text-right fw-400">{(temp.main.feels_like - 273.15).toFixed(2)}º</p>
+                            }
+                        </div>
+                    </div>
+
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Minimum</p>
+                        </div>
+                        <div className="d-grid-3">                        
+                            {
+                                temp && temp.main && temp.main.temp_min && // IF statement
+                                <p className="text-right fw-400">{(temp.main.temp_min - 273.15).toFixed(2)}º</p>
+                            }
+                        </div>
+                    </div>
 
 
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Temp. Maximum</p>
-                            </div>
-                            <div className="d-grid-3">
-                                {
-                                    temp && temp.main && temp.main.temp_max && // IF statement
-                                    <p className="text-right">{(temp.main.temp_max - 273.15).toFixed(2)}º</p>
-                                }
-                            </div>
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Maximum</p>
                         </div>
+                        <div className="d-grid-3">
+                            {
+                                temp && temp.main && temp.main.temp_max && // IF statement
+                                <p className="text-right fw-400">{(temp.main.temp_max - 273.15).toFixed(2)}º</p>
+                            }
+                        </div>
+                    </div>
 
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Pressure</p>
-                            </div>
-                            <div className="d-grid-3">
-                                {
-                                    temp && temp.main && temp.main.pressure && // IF statement
-                                    <p className="text-right">{temp.main.pressure}Pa</p>
-                                }
-                            </div>
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Pressure</p>
                         </div>
+                        <div className="d-grid-3">
+                            {
+                                temp && temp.main && temp.main.pressure && // IF statement
+                                <p className="text-right fw-400">{temp.main.pressure}Pa</p>
+                            }
+                        </div>
+                    </div>
 
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Humidity</p>
-                            </div>
-                            <div className="d-grid-3">
-                                {
-                                    temp && temp.main && temp.main.humidity && // IF statement
-                                    <p className="text-right">{temp.main.humidity}%</p>
-                                }
-                            </div>
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Humidity</p>
                         </div>
+                        <div className="d-grid-3">
+                            {
+                                temp && temp.main && temp.main.humidity && // IF statement
+                                <p className="text-right fw-400">{temp.main.humidity}%</p>
+                            }
+                        </div>
+                    </div>
 
-                        <div className="d-grid">
-                            <div className="d-grid-1-3">
-                                <p className="text-left">Wind</p>
-                            </div>
-                            <div className="d-grid-3">
-                                {
-                                    temp && temp.wind && temp.wind.speed && // IF statement
-                                    <p className="text-right">{temp.wind.speed}km/h</p>
-                                }
-                            </div>
+                    <div className="d-grid">
+                        <div className="d-grid-1-3">
+                            <p className="text-left fw-400">Wind</p>
                         </div>
+                        <div className="d-grid-3">
+                            {
+                                temp && temp.wind && temp.wind.speed && // IF statement
+                                <p className="text-right fw-400">{temp.wind.speed}km/h</p>
+                            }
+                        </div>
+                    </div>
                 </section>
-
+                }
             </div>
         </div>
     );
