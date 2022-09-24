2d5e56419de2f69e7fa73324fa22f7ae15d5cd38
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2022-09-24 16:30:20 +0100

    Unused files removed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,9 @@
 import './App.css';
-import Nav from './components/Nav'; // Import Nav from Nav.js
 import Temperature from './components/Temperature'; // Import Temperature from Temperature.js
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="h-100">
-      {/* <Router>
-        <div className="App">
-          <header className="App-header">
-            <Nav />
-            <Routes>
-              <Route path="/" />
-              <Route path="/temperature" element={<Temperature />} />
-            </Routes>
-          </header>
-        </div>
-      </Router> */}
-      
       <Temperature />
     </div>
   );
